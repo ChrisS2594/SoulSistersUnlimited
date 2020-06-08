d53f596a4ca18d1f7fb761e8e6258aeb6d93c08f
Chris <stephens-c2@live.com>
2020-06-08 15:21:31 -0700

    adding update:

---
+++ soul-sisters/src/Components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.scss";
+import { BrowserRouter as Router,
+Switch,
+Route,
+Link,
+useRouteMatch,
+useParams } from "react-router-dom";
+
+function Nav() {
+return (
+    
+)
+}
