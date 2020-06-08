11c314d5b1f1790f98c419c9f861e4da7c13694e
Chris <stephens-c2@live.com>
2020-06-08 16:57:22 -0700

    creating basing application frame

--- soul-sisters/src/App.js
+++ soul-sisters/src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import Nav from './Components/Nav/Nav';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
+//  import logo from "Logo.jpg"; Working to import logo in the future.
+import Logo from "./Logo.jpg";
 
+
+// return <img src ={logo} alt= "Logo" />
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-      <h1>Crafting. Creating. Loving Every Minute Of It.</h1>
-      </header>
-    </div>
+
+    <Router>
+
+      <div className="App">
+        <header className="App-header">
+          <img src={Logo} className="Logo" alt="Logo"></img>
+          <Nav />
+          <h1 id="head">Crafting. Creating. Loving Every Minute Of It!</h1>
+        </header>
+      </div>
+    </Router>
   );
 }
 
--- soul-sisters/src/Components/Nav/Nav.js
+++ soul-sisters/src/Components/Nav/Nav.js
@@ -9,6 +9,43 @@ useParams } from "react-router-dom";
 
 function Nav() {
 return (
-    
-)
+    <nav id="nav" role="navigation">
+    <a href="#nav" title="Show navigation">Show navigation</a>
+    <a href="#" title="Hide navigation">Hide navigation</a>
+
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <a href="/" aria-haspopup="true"><span>Products</span></a>
+        <ul>
+        <li><Link to="#">Tumblers</Link></li>
+        <li><Link to="#">Plates/Trays</Link></li>
+        <li><Link to="#">Shirts</Link></li>
+        <li><Link to="#">Bags</Link></li>
+        <li><Link to ="#">Custom Requests</Link></li>
+        
+        </ul>
+        </li>
+
+      <li>
+        <Link to="#">Order Status</Link>
+      </li>
+      <li>
+        <a href="/" aria-haspopup="true"><span>Login</span></a>
+        <ul>
+        <li><Link to="#">Customer Login</Link></li>
+        <li><Link to="#">Owner Login</Link></li>
+        </ul>
+        </li>
+    </ul>
+
+  </nav>
+
+);
+
 }
+
+
+export default Nav;
