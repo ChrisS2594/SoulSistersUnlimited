a1ed19ccfdf558064c78293b1a21205fff29f963
Chris <stephens-c2@live.com>
2020-06-08 17:27:18 -0700

    creating splash page

--- soul-sisters/src/App.js
+++ soul-sisters/src/App.js
@@ -11,13 +11,14 @@ import {
 } from "react-router-dom";
 //  import logo from "Logo.jpg"; Working to import logo in the future.
 import Logo from "./Logo.jpg";
-
+import Splash from "./Pages/splash";
 
 // return <img src ={logo} alt= "Logo" />
 function App() {
   return (
 
     <Router>
+     
 
       <div className="App">
         <header className="App-header">
@@ -26,6 +27,7 @@ function App() {
           <h1 id="head">Crafting. Creating. Loving Every Minute Of It!</h1>
         </header>
       </div>
+      <Route exact path="/" component={Splash} />
     </Router>
   );
 }
---
+++ soul-sisters/src/Pages/splash.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./../App.scss";
+import {
+    BrowserRouter as Router,
+    Link,
+  
+    //BrowserHistory
+  } from "react-router-dom";
+  function Splash() {
+    return (
+    
+        <div className="about">
+            <h3>About Us</h3>
+            <p>Our mission is to creatively inspire other soul sisters and misters with our handmade products, organizing solutions and inspirational videos and posts.</p>
+        <p>Handmade Tumblers, Cups, Signs, shirts and more! We can customize just about anything you want!</p>
+        
+        </div>
+      
+     
+    ); 
+  }
+  
+  export default Splash;
